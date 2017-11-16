0b4531ffed820ab7d23b175a20527866f9caa940
andrew-stephens <andrew.stephens@ametek.com>
2017-11-16 15:44:47 +0000

    Commit foobar

--- MyTestProject/MyTestProject/Program.cs
+++ MyTestProject/MyTestProject/Program.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyTestProject
+﻿namespace MyTestProject
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // This is my first change to the file
             // A second change
+
+            // Another change after first commit
         }
     }
 }
